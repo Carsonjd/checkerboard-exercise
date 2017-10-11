cd004f15074e0d1f031a804c9c420365abf1e091
Jonathan Carson <carsonjd@colorado.edu>
2017-10-11 13:04:50 -0600

    red black check com

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Your JS goes here
+// document.addEventListener('DOMContentLoaded', function () {
+// });
+
+for(var i=0;i<81;i++){
+var tile = document.createElement('div');
+var body = document.getElementsByTagName('body')[0];
+document.body.prepend(tile);
+tile.style.width = '11.1%';
+tile.style.paddingBottom = '11.1%';
+tile.display = 'float';
+tile.style.float = 'left';
+if(i%2 === 0){
+tile.style.backgroundColor = 'red';
+}
+if(i%2 !== 0){
+tile.style.backgroundColor = 'black';
+}
+};
