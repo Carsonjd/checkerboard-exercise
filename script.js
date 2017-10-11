8f71acdcc64a2e178ae45462884256e5b2347d18
Jonathan Carson <carsonjd@colorado.edu>
2017-10-11 13:14:11 -0600

    rando final

--- script.js
+++ script.js
@@ -3,17 +3,18 @@
 // });
 
 for(var i=0;i<81;i++){
-var tile = document.createElement('div');
-var body = document.getElementsByTagName('body')[0];
-document.body.prepend(tile);
-tile.style.width = '11.1%';
-tile.style.paddingBottom = '11.1%';
-tile.display = 'float';
-tile.style.float = 'left';
-if(i%2 === 0){
-tile.style.backgroundColor = 'red';
-}
-if(i%2 !== 0){
-tile.style.backgroundColor = 'black';
-}
+  var randoC = '#' + Math.ceil(1000000 * Math.random())
+  var tile = document.createElement('div');
+  var body = document.getElementsByTagName('body')[0];
+  document.body.prepend(tile);
+  tile.style.width = '11.1%';
+  tile.style.paddingBottom = '11.1%';
+  tile.display = 'float';
+  tile.style.float = 'left';
+    if(i%2 === 0){
+        tile.style.backgroundColor = randoC;
+        }
+    if(i%2 !== 0){
+      tile.style.backgroundColor = randoC;
+    }
 };
